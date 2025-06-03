96878d3513c6c551551a6b25da529ef6be9ce476
Тетяна <yabanshi.tania@gmail.com>
2025-06-03 23:00:16 +0300

    update index html file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,13 +3,10 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = "feedback-form-state";
 
-// Ініціалізація об'єкта formData
 let formData = {
   email: "",
   message: "",
 };
-
-// Завантаження збережених даних з локального сховища
 const savedData = localStorage.getItem(STORAGE_KEY);
 if (savedData) {
   try {
@@ -21,14 +18,12 @@ if (savedData) {
   }
 }
 
-// Обробник події input для збереження даних у локальне сховище
 form.addEventListener("input", (evt) => {
   const { name, value } = evt.target;
   formData[name] = value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
-// Обробник події submit для перевірки та очищення даних
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
 
